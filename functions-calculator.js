9ba5ef5b2e310cfa2284ec56649b4cc9b751f8b8
Isaac_laptop <nachoartaviasalazar@gmail.com>
2025-03-07 10:14:51 -0600

    JavaScript usable

--- functions-calculator.js
+++ functions-calculator.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("sumForm").addEventListener("submit",function(event){
+        event.preventDefault();
+        sum();
+    });
+
+});
+
+
 function sum(){
-    
+    console.log("sumarrrrrrrrrrrrrrr")
+    let num1 = parseFloat(document.getElementById("num1").value) || 0;
+    let num2 = parseFloat(document.getElementById("num2").value) || 0;
+    let resultado = num1 + num2;
+    document.getElementById("resultado").textContent = resultado;
 }
